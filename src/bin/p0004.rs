0349edcf37919b79edbbf328687cfab567dd5ae1
trevor <tmbarker9@gmail.com>
2024-01-17 16:18:45 -0800

    Solve p0004

---
+++ src/bin/p0004.rs
@@ -0,0 +1,33 @@
+use utils::integers::Integer;
+
+fn main() {
+    println!("Problem 4: Largest Palindrome Product => {0}", largest_palindrome_product(3));
+}
+
+const RADIX: usize = 10;
+
+fn largest_palindrome_product(d: u32) -> usize {
+    let min = RADIX.pow(d - 1);
+    let max = RADIX.pow(d);
+
+    let mut p: usize;
+    let mut n: usize = 0;
+
+    for a in (min..max).rev() {
+        for b in (a..max).rev() {
+            p = a * b;
+            if p <= n {
+                break;
+            }
+            if p.is_palindromic(RADIX) {
+                n = std::cmp::max(n, p);
+            }
+        }
+    }
+    return n;
+}
+
+#[test]
+fn validate() {
+    assert_eq!(largest_palindrome_product(3), 906609);
+}
