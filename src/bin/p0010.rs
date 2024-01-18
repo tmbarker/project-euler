19bbfd03c2a8e5d8eec61658cba3c773e2f9a17b
trevor <tmbarker9@gmail.com>
2024-01-18 12:59:44 -0800

    Solve p0010

---
+++ src/bin/p0010.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!(
+        "Problem 10: Summation of Primes => {0}",
+        sum_primes(2000000)
+    );
+}
+
+fn sum_primes(n: u64) -> u64 {
+    utils::primes::PrimeGen::new()
+        .iter()
+        .take_while(|&p| p < n)
+        .sum()
+}
+
+#[test]
+fn validate() {
+    assert_eq!(sum_primes(2000000), 142913828922)
+}
