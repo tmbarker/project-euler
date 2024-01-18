e67d97669be65ef1e3a2df989591f0cf2e269ce1
trevor <tmbarker9@gmail.com>
2024-01-17 16:53:18 -0800

    Solve p0005

---
+++ src/bin/p0005.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Problem 5: Smallest Multiple => {0}", smallest_multiple(20));
+}
+
+fn smallest_multiple(x: usize) -> usize {
+    (1..(x + 1)).fold(1, |lcm, n| num_integer::lcm(lcm, n))
+}
+
+#[test]
+fn validate() {
+    assert_eq!(smallest_multiple(20), 232792560);
+}
