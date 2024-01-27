ee8d946f71cd45876853ff15cddfd4f1d51a879b
trevor <tmbarker9@gmail.com>
2024-01-26 20:47:55 -0800

    Add num_bigint crate, solve p0020

---
+++ src/bin/p0020.rs
@@ -0,0 +1,25 @@
+use num_bigint::BigUint;
+
+fn main() {
+    println!(
+        "Problem 20: Factorial Digit Sum => {0}",
+        factorial_digit_sum(100)
+    );
+}
+
+fn factorial_digit_sum(x: u32) -> u32 {
+    (1..=x)
+        .map(|n| BigUint::from(n))
+        .fold(num_traits::one::<BigUint>(), |acc, n| acc * n)
+        .to_string()
+        .chars()
+        .filter_map(|c| c.to_digit(10))
+        .sum()
+}
+
+#[test]
+fn validate() {
+    assert_eq!(factorial_digit_sum(1), 1);
+    assert_eq!(factorial_digit_sum(10), 27);
+    assert_eq!(factorial_digit_sum(100), 648);
+}
