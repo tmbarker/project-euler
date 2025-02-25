eb48b2134955986f6eb1db1fbc1b1a5ed2c8dd7a
Trevor Barker <tmbarker9@gmail.com>
2025-02-25 14:07:09 -0800

    Solve p0036

---
+++ src/bin/p0036.rs
@@ -0,0 +1,10 @@
+﻿use integer::Integer;
+
+fn solve() -> String {
+    (1..1_000_000)
+        .filter(|&n| n.is_palindromic(10) && n.is_palindromic(2))
+        .sum::<usize>()
+        .to_string()
+}
+
+euler::register_problem!("Double-base Palindromes", solve, "872187");
