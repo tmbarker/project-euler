2b4727735440c670207343fde45a3944150e0ad6
Trevor Barker <tmbarker9@gmail.com>
2025-02-25 22:30:33 -0800

    Solve p0042

---
+++ src/bin/p0042.rs
@@ -0,0 +1,27 @@
+﻿use std::collections::HashSet;
+use seq::TriangularIter;
+
+fn solve(input: &str) -> String {
+    let words = input
+        .split(',')
+        .map(|name| name.trim_matches('"'))
+        .map(|name| name
+            .to_ascii_uppercase()
+            .bytes()
+            .map(|c| (c - b'A' + 1) as usize)
+            .sum::<usize>())
+        .collect::<Vec<_>>();
+
+    let max = words.iter().max().unwrap();
+    let tri = TriangularIter::new()
+        .take_while(|&t| t <= *max)
+        .collect::<HashSet<usize>>();
+
+    words
+        .iter()
+        .filter(|&word| tri.contains(word))
+        .count()
+        .to_string()
+}
+
+euler::register_problem!("Coded Triangle Numbers", "0042_words.txt", solve, "162");
