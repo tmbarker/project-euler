dd28046552f53574cac769d5ca2d0b6a56cf131d
Trevor Barker <tmbarker9@gmail.com>
2025-02-25 23:19:52 -0800

    Solve p0047

---
+++ src/bin/p0047.rs
@@ -0,0 +1,15 @@
+﻿use prime::{Factorize, PrimeSeq};
+
+fn solve() -> String {
+    let ps = PrimeSeq::new();
+    (1..)
+        .find(|&n| {
+            (n..n + 4)
+                .map(|n| n.factorize(&ps).count())
+                .all(|num_factors| num_factors == 4)
+        })
+        .unwrap()
+        .to_string()
+}
+
+euler::register_problem!("Distinct Primes Factors", solve, "134043");
