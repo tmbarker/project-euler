233a8c8e56b664e9e6c75cd615d9011402d3f29b
Trevor Barker <tmbarker9@gmail.com>
2025-03-06 22:01:11 -0800

    Solve p0052

---
+++ src/bin/p0052.rs
@@ -0,0 +1,29 @@
+﻿use integer::Integer;
+use std::collections::HashSet;
+
+const RADIX: usize = 10;
+const MULTIPLES: &[usize] = &[2, 3, 4, 5, 6];
+
+fn solve() -> String {
+    (1..)
+        .find(|&n| check(n))
+        .unwrap()
+        .to_string()
+}
+
+fn check(n: usize) -> bool {
+    let a: Vec<_> = (n * MULTIPLES[0]).into_digits(RADIX).collect();
+    (1..MULTIPLES.len())
+        .all(|i| {
+            let b: Vec<_> = (n * MULTIPLES[i]).into_digits(RADIX).collect();
+            same_elements(&a, &b)
+        })
+}
+
+fn same_elements(a: &[usize], b: &[usize]) -> bool {
+    let set_a: HashSet<_> = a.iter().collect();
+    let set_b: HashSet<_> = b.iter().collect();
+    set_a == set_b
+}
+
+euler::register_problem!("Permuted Multiples", solve, "142857");
